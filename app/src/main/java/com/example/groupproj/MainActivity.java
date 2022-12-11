544a628d8fffaf75fc799ca28950527817f16ce7
Banu500 <elmiibrahim@gmail.com>
2022-12-11 01:50:34 -0500

    splash screen added

--- app/src/main/java/com/example/groupproj/MainActivity.java
+++ app/src/main/java/com/example/groupproj/MainActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
-import java.util.logging.Handler;
+import android.os.Handler;
+
+
+
 import java.util.logging.LogRecord;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +19,28 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+//        new  Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                // on below line we are
+//                // creating a new intent
+//                Intent i = new Intent(MainActivity.this, AboutActivity.class);
+//
+//                // on below line we are
+//                // starting a new activity.
+//                startActivity(i);
+//
+//                // on the below line we are finishing
+//                // our current activity.
+//                finish();
+//            }
+//        }, 2000);
+
+
         Button btn = (Button)findViewById(R.id.add_button);
         Button btn2 = (Button)findViewById(R.id.about_button);
 

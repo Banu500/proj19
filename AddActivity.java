68012a9237f9e49e80483c9a5cc7c142c1b49924
Maoe1 <62967663+Maoe1@users.noreply.github.com>
2022-12-11 14:19:33 -0500

    Add files via upload
    add activity and layout updated

---
+++ AddActivity.java
@@ -0,0 +1,72 @@
+package com.example.groupproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddActivity extends AppCompatActivity {
+    RatingBar ratingbar;
+    ListView show;
+    public ArrayList<Resturant> resturants = new ArrayList<Resturant>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        EditText name = findViewById(R.id.name_id);
+        EditText address = findViewById(R.id.address_id);
+        EditText tags = findViewById(R.id.Tags);
+        EditText desc = findViewById(R.id.desc_id);
+        ratingbar = (RatingBar)findViewById(R.id.ratingbar_id);
+        TextView out = findViewById(R.id.save_id);
+        Button btn = findViewById(R.id.back_id);
+
+        out.setOnClickListener(view -> {
+            String name_input = name.getText().toString();
+            String address_input = address.getText().toString();
+            String tags_input = tags.getText().toString();
+            String desc_input = desc.getText().toString();
+            String rating =String.valueOf(ratingbar.getRating());
+
+            Resturant res = new Resturant(name_input, address_input);
+
+            resturants.add(res);
+            //Resturant.add(tags_input);
+            //Resturant.add(desc_input);
+            //Resturant.add(rating);
+
+            Intent intent = new Intent(AddActivity.this, MainActivity.class);
+            intent.putExtra("key", resturants);
+            startActivity(intent);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        });
+
+
+    }
+}

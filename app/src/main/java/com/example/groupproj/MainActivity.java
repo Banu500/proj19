aabb38fee67005067a70f870aa1040a9e79b8417
Banu500 <elmiibrahim@gmail.com>
2022-12-10 23:40:38 -0500

    btns

--- app/src/main/java/com/example/groupproj/MainActivity.java
+++ app/src/main/java/com/example/groupproj/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.groupproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button)findViewById(R.id.add_button);
+        Button btn2 = (Button)findViewById(R.id.about_button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AddActivity.class));
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, AboutActivity.class));
+            }
+        });
+
+        }
+
     }
-}

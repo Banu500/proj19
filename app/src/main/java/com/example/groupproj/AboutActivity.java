3f30aa7be4830b29d13189cab1893047cdfbc7d3
Banu500 <elmiibrahim@gmail.com>
2022-12-11 00:53:50 -0500

    details screen

--- app/src/main/java/com/example/groupproj/AboutActivity.java
+++ app/src/main/java/com/example/groupproj/AboutActivity.java
@@ -3,12 +3,29 @@ package com.example.groupproj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
     }
 }

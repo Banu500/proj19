320b062789be04350d6326767d4b1d3fe5115eb5
Banu500 <elmiibrahim@gmail.com>
2022-12-11 14:35:02 -0500

    location screen edit

--- app/src/main/java/com/example/groupproj/LocationActivity.java
+++ app/src/main/java/com/example/groupproj/LocationActivity.java
@@ -2,9 +2,12 @@ package com.example.groupproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class LocationActivity extends AppCompatActivity {
 
@@ -13,6 +16,15 @@ public class LocationActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+        Button btn = (Button)findViewById(R.id.home1_btn);
+        Button btn2 = (Button)findViewById(R.id.directions_btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LocationActivity.this, MainActivity.class));
+            }
+        });
 
     }
     @Override

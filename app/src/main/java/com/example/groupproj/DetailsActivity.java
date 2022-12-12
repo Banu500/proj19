e09e539a39252710d202c379e20433e5670fe7d5
Banu500 <elmiibrahim@gmail.com>
2022-12-12 04:57:28 -0500

    Added map functionality

--- app/src/main/java/com/example/groupproj/DetailsActivity.java
+++ app/src/main/java/com/example/groupproj/DetailsActivity.java
@@ -3,7 +3,9 @@ package com.example.groupproj;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,11 +23,23 @@ public class DetailsActivity extends AppCompatActivity {
         Button btn2 = (Button)findViewById(R.id.home1_btn);
 
         btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(DetailsActivity.this, LocationActivity.class));
-            }
-        });
+                                   @Override
+
+                                   public void onClick(View v) {
+                                       new Handler().postDelayed(new Runnable() {
+
+                                           @Override
+                                           public void run() {
+                                               Uri gmmIntentUri = Uri.parse("geo:0,0?q=196 Bloor St");
+                                               Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                                               mapIntent.setPackage("com.google.android.apps.maps");
+                                               startActivity(mapIntent);
+                                           }
+                                       }, 1000);
+                                   }
+                               });
+
+
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
